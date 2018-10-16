a8317c54597f0af0b1655758cda85cc5f97d9332
Eva <evaliisagalen@gmail.com>
2018-10-15 21:56:47 -0700

    minor style fixes

--- src/containers/Project.js
+++ src/containers/Project.js
@@ -3,10 +3,12 @@ import { withRouteData, Link } from 'react-static'
 //
 
 export default withRouteData(({ project }) => (
-  <div>
-    <Link to="/work/">{'<'} Back</Link>
-    <br />
-    <h3>{project.title}</h3>
-    <p>{project.description}</p>
+  <div className="row justify-content-center">
+    <div className="col-sm-10">
+      <Link to="/work/">{'<'} Back</Link>
+      <br />
+      <h3>{project.title}</h3>
+      <p>{project.description}</p>
+    </div>
   </div>
 ))
--- src/containers/Work.js
+++ src/containers/Work.js
@@ -4,16 +4,18 @@ import { withRouteData, Link } from 'react-static'
 //
 
 export default withRouteData(({ projects }) => (
-  <div>
-    <h1>Work</h1>
-    <br />
-    All Projects:
-    <ul>
-      { projects.map(project => (
-        <li key={project.id}>
-          <Link to={`/work/project/${project.id}/`}>{project.title}</Link>
-        </li>
-      ))}
-    </ul>
+  <div className="row justify-content-center">
+    <div className="col-sm-10">
+      <h1>Work</h1>
+      <br />
+      All Projects:
+      <ul>
+        { projects.map(project => (
+          <li key={project.id}>
+            <Link to={`/work/project/${project.id}/`}>{project.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   </div>
 ))
